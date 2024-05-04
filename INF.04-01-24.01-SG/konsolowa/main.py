5907ee394d767bd5c9fb3214a047de6b9e0f1504
ZYGA1 <j.zynowski123@gmail.com>
2024-05-04 23:51:48 +0200

    konsolowa

--- INF.04-01-24.01-SG/konsolowa/main.py
+++ INF.04-01-24.01-SG/konsolowa/main.py
@@ -1,23 +1,34 @@
-arr = input("podaj pesel: ")
-if len(arr) != 11:
-    arr = input("podaj poprawnyn pesel: ")
-def sol(arr):
-    S = 0
-    waga = [1,3,7,9,1,3,7,9,1,3]
-    for i in range(len(arr) - 1):
-        S += int(arr[i]) * waga[i]
-    M = S % 10
-    if M ==0:
-        R = 0
-    else:
-        R = 10 - M
-    if R == int(arr[-1]):
-        return True
-    return False
-def plec(arr):
-    if int(arr[10]) % 2 == 0:
-        return 'M' 
-    else:
-        return 'F'
 
-print(f'{sol(arr)}, {plec(arr)}')
+class Pesel:
+
+    def __init__(self, pesel):
+        self.s = pesel
+    
+    def getInf(self):
+        print(f'{self.sol(self.s)}, {self.plec(self.s)}')
+
+    def sol(self, arr):
+        S = 0
+        waga = [1,3,7,9,1,3,7,9,1,3]
+        for i in range(len(arr) - 1):
+            S += int(arr[i]) * waga[i]
+        M = S % 10
+        if M ==0:
+            R = 0
+        else:
+            R = 10 - M
+        if R == int(arr[-1]):
+            return True
+        return False
+    def plec(self, arr):
+        if int(arr[10]) % 2 == 0:
+            return 'M' 
+        else:
+            return 'F'
+
+
+if __name__ ==  '__main__':
+    pesel = Pesel('55030101193')
+    pesel.getInf()
+    
+
